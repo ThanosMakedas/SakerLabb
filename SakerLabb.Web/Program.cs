e87e2ef82bb0247c9d20f94f16900cc0941799ec
ThanosMakedas <makedasathanasios@gmail.com>
2026-08-31 20:11:46 +0200

    Fynd 4: lagg till sakerhetsheaders
    ZAP-larm CSP Header Not Set (Medium), Missing Anti-clickjacking
    Header (Medium) och X-Content-Type-Options Header Missing (Low).
    Appen satte inga sakerhetsheaders alls. Nu satts CSP,
    X-Content-Type-Options, X-Frame-Options och Referrer-Policy.

--- SakerLabb.Web/Program.cs
+++ SakerLabb.Web/Program.cs
@@ -29,6 +29,36 @@ var app = builder.Build();
 
 app.Services.GetRequiredService<Db>().Initialize();
 
+// Sakerhetsheaders. Satts i OnStarting sa att de kommer med aven pa
+// felsidor, dar svaret annars nollstalls och headers forsvinner.
+app.Use(async (context, next) =>
+{
+    context.Response.OnStarting(() =>
+    {
+        var headers = context.Response.Headers;
+
+        headers["Content-Security-Policy"] =
+            "default-src 'self'; " +
+            "script-src 'self'; " +
+            "style-src 'self'; " +
+            "img-src 'self' data:; " +
+            "font-src 'self'; " +
+            "connect-src 'self'; " +
+            "object-src 'none'; " +
+            "frame-ancestors 'none'; " +
+            "base-uri 'self'; " +
+            "form-action 'self'";
+
+        headers["X-Content-Type-Options"] = "nosniff";
+        headers["X-Frame-Options"] = "DENY";
+        headers["Referrer-Policy"] = "no-referrer";
+
+        return Task.CompletedTask;
+    });
+
+    await next();
+});
+
 app.UseDeveloperExceptionPage();
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 
